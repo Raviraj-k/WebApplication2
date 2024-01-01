678e27c7a860a3c229bb0cb5651aaa24bddd135a
HP <HP@HEY-RAJ->
2024-01-01 14:32:17 +0530

    Add project files.

---
+++ WebApplication2/DTO/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace WebApplication2.DTO
+{
+    public class TaskModel
+    {
+        [JsonProperty(PropertyName = "taskName", NullValueHandling = NullValueHandling.Ignore)]
+        public string TaskName { get; set; }
+
+        [JsonProperty(PropertyName = "taskDescription", NullValueHandling = NullValueHandling.Ignore)]
+        public string TaskDescription { get; set; }
+    }
+}
